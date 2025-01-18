9d668a949e937252c2975675e1dce72f5d10268d
Tanmay <tanmaysharma763@gmail.com>
2025-01-19 01:10:02 +0530

    wdad32

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn, SignUp } from '@clerk/nextjs'
 export default function Page() {
   return( 
     <section className='flex justify-center items-center w-full h-screen'>
-   <SignIn signUpForceRedirectUrl="/" />
+   <SignIn />
   
   </section>
   )
