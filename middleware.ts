e995e8b22c75cc5f7d01865552b48c1ca4f8826f
Tanmay <tanmaysharma763@gmail.com>
2025-01-19 00:37:02 +0530

    middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,19 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  // An array of public routes that don't require authentication.
+  publicRoutes: ["/api/webhook/clerk"],
+
+  // An array of routes to be ignored by the authentication middleware.
+  ignoredRoutes: ["/api/webhook/clerk"],
+})
+
 
 export const config = {
   matcher: [
-    
-    '/',
-    '/api/webhook/clerk',
-   
+    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-};
+}
