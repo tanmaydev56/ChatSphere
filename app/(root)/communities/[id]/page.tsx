d9793a6d2eecf5f016afd9af16ea6845bc015d2a
Tanmay <tanmaysharma763@gmail.com>
2025-01-19 00:03:51 +0530

    vercel error

--- app/(root)/communities/[id]/page.tsx
+++ app/(root)/communities/[id]/page.tsx
@@ -1,88 +1,71 @@
-import Image from "next/image";
 import { currentUser } from "@clerk/nextjs/server";
-
-import { communityTabs } from "@/constants";
-
-import UserCard from "@/components/cards/UserCard";
+import { redirect } from "next/navigation";
+import { profileTabs } from "@/constants";
 import ThreadsTab from "@/components/shared/ThreadsTab";
 import ProfileHeader from "@/components/shared/ProfileHeader";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { fetchUser } from "@/lib/actions/user.actions";
 
-import { fetchCommunityDetails } from "@/lib/actions/community.actions";
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
 
-async function Page({ params }: { params: { id: string } }) {
+async function Page({ params }: PageProps) {
   const user = await currentUser();
   if (!user) return null;
 
-  const communityDetails = await fetchCommunityDetails(params.id);
+  const userInfo = await fetchUser(params.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
   return (
     <section>
       <ProfileHeader
-        accountId={communityDetails.createdBy.id}
+        accountId={userInfo.id}
         authUserId={user.id}
-        name={communityDetails.name}
-        username={communityDetails.username}
-        imgUrl={communityDetails.image}
-        bio={communityDetails.bio}
-        type='Community'
+        name={userInfo.name}
+        username={userInfo.username}
+        imgUrl={userInfo.image}
+        bio={userInfo.bio}
       />
 
-      <div className='mt-9'>
-        <Tabs defaultValue='threads' className='w-full'>
-          <TabsList className='tab'>
-            {communityTabs.map((tab) => (
-              <TabsTrigger key={tab.label} value={tab.value} className='tab'>
+      <div className="mt-9">
+        <Tabs defaultValue="threads" className="w-full">
+          <TabsList className="tab">
+            {profileTabs.map((tab) => (
+              <TabsTrigger key={tab.label} value={tab.value} className="tab">
                 <Image
                   src={tab.icon}
                   alt={tab.label}
                   width={24}
                   height={24}
-                  className='object-contain'
+                  className="object-contain"
                 />
-                <p className='max-sm:hidden'>{tab.label}</p>
+                <p className="max-sm:hidden">{tab.label}</p>
 
                 {tab.label === "Threads" && (
-                  <p className='ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2'>
-                    {communityDetails.threads.length}
+                  <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
+                    {userInfo.threads.length}
                   </p>
                 )}
               </TabsTrigger>
             ))}
           </TabsList>
-
-          <TabsContent value='threads' className='w-full text-light-1'>
-            {/* @ts-ignore */}
-            <ThreadsTab
-              currentUserId={user.id}
-              accountId={communityDetails._id}
-              accountType='Community'
-            />
-          </TabsContent>
-
-          <TabsContent value='members' className='mt-9 w-full text-light-1'>
-            <section className='mt-9 flex flex-col gap-10'>
-              {communityDetails.members.map((member: any) => (
-                <UserCard
-                  key={member.id}
-                  id={member.id}
-                  name={member.name}
-                  username={member.username}
-                  imgUrl={member.image}
-                  personType='User'
-                />
-              ))}
-            </section>
-          </TabsContent>
-
-          <TabsContent value='requests' className='w-full text-light-1'>
-            {/* @ts-ignore */}
-            <ThreadsTab
-              currentUserId={user.id}
-              accountId={communityDetails._id}
-              accountType='Community'
-            />
-          </TabsContent>
+          {profileTabs.map((tab) => (
+            <TabsContent
+              key={`content-${tab.label}`}
+              value={tab.value}
+              className="w-full text-light-1"
+            >
+              {/* @ts-ignore */}
+              <ThreadsTab
+                currentUserId={user.id}
+                accountId={userInfo.id}
+                accountType="User"
+              />
+            </TabsContent>
+          ))}
         </Tabs>
       </div>
     </section>
